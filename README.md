e81316e4d1ae22a6fe6c9904c68ccb3248ce7e14
k-sunako <dd.kazumasa@gmail.com>
2026-04-26 15:42:44 +0900

    commit.

--- README.md
+++
@@ -1,9 +0,0 @@
-# dotfiles
-manage my dotfiles.
-
-## .emacs.d
-Emacsの設定
-
-## .tmux.conf
-ひとまずコミット
-x
--- texmf/tex/lualatex/auto/my.el
+++ texmf/tex/lualatex/auto/my.el
@@ -1,9 +1,29 @@
+;; -*- lexical-binding: t; -*-
+
 (TeX-add-style-hook
  "my"
  (lambda ()
    (setq TeX-command-extra-options
          "-shell-escape -interaction nonstopmode --synctex=1")
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("tcolorbox" "") ("amsmath" "") ("amsthm" "") ("mathrsfs" "") ("amssymb" "") ("xifthen" "") ("xstring" "") ("physics" "") ("tikz" "") ("tikz-cd" "") ("minted" "") ("luatex85" "") ("luatexja-fontspec" "")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimWrite")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbEnv")
+   (add-to-list 'LaTeX-verbatim-environments-local "SaveVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "VerbatimOut")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "LVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "BVerbatim")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim*")
+   (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "Verb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "EscVerb")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "EscVerb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb*")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "Verb")
    (TeX-run-style-hooks
     "tcolorbox"
     "amsmath"
@@ -21,6 +41,9 @@
    (TeX-add-symbols
     '("xleftleftarrows" ["argument"] 1)
     '("xrightrightarrows" ["argument"] 1)
+    "dom"
+    "cod"
+    "sign"
     "relrelbarsep"
     "relrelbar"
     "rightrightarrowsfill"
@@ -35,15 +58,6 @@
     "lemma"
     "definition")
    (LaTeX-add-amsthm-newtheoremstyles
-    "break")
-   (LaTeX-add-tcolorbox-lib-theorems-newtcbtheorems
-    "defn"
-    "theo"
-    "lemm"
-    "clai"
-    "coro"
-    "exam"
-    "algo"
-    "rema"))
+    "break"))
  :latex)
 
